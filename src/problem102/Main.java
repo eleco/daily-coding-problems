4ec816bb6a68b1f405d1ab551fc67b54da3d1982
eleco <eric.leconte@gmail.com>
2018-06-18 20:11:16 +0100

    problem 102 sum subarray

---
+++ src/problem102/Main.java
@@ -0,0 +1,56 @@
+package problem102;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Given a list of integers and a number K, return which contiguous elements of the list sum to K.
+        // For example, if the list is [1, 2, 3, 4, 5] and K is 9, then it should return [2, 3, 4].
+
+        System.out.println(window_brute(new int[]{1, 2, 3, 4, 5}, 9));
+        System.out.println(window_optimized(new int[]{1, 2, 3, 4, 5}, 9));
+    }
+
+    static List<Integer> window_optimized(int[] arr, int k) {
+
+        List<Integer> ret = new ArrayList<Integer>();
+        int sum = 0;
+        int trailerIdx = 0;
+        for (int anArr : arr) {
+            sum += anArr;
+            ret.add(anArr);
+            if (sum > k) {
+                sum -= arr[trailerIdx++];
+                ret.remove(0);
+            }
+            if (sum == k) {
+                return ret;
+            }
+        }
+        return Collections.emptyList();
+    }
+
+
+    static List<Integer> window_brute(int[] arr, int k) {
+
+        List<Integer> ret = new ArrayList<Integer>();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            ret.add(arr[i]);
+            if (sum == k) return ret;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                sum += arr[j];
+                ret.add(arr[j]);
+                if (sum == k) return ret;
+            }
+            sum = 0;
+            ret.clear();
+        }
+        return ret;
+    }
+}
