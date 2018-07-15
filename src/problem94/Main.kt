a3796d21b7fdabda8cf10c98f7076c6fc1bed46a
ericl <eric.leconte@gmail.com>
2018-07-15 13:47:50 +0100

    problem94 amend solution

--- src/problem94/Main.kt
+++ src/problem94/Main.kt
@@ -10,16 +10,30 @@ object Main {
         // The path must go through at least one node, and does not need to go through the root.
 
         //Approach
+
+        //v1 traverse() + maxSumToRoot()
         //recursively traverse the tree
         //for each node traversed, compute the maximum path of the right + left sub tree for the current node
+
+        //v2 maxSumToRootV2
+        //recursively traverse the tree
+        //for each node: calculate max path of left and right sub trees
+        //update global max sum path if either left subtree, or right subtree, or left+root+right subtree exceed current max
+        //depending on wether max sum path goes through the root or not
+
+
+
         traverse(buildSampleTree())
+        println("max path sum:" + max)
+
+        max=0;
+        maxSumToRootv2(buildSampleTree())
         println("max path sum:" + max)
     }
 
 
     fun traverse(node: BinaryTree<Int>) {
         var tmp = node.data;
-
 
         val right = node.right;
         if (right != null) {
@@ -37,8 +51,29 @@ object Main {
 
     }
 
-    fun maxSumToRoot(node: BinaryTree<Int>): Int {
 
+    fun maxSumToRootv2(node: BinaryTree<Int>): Int {
+        if (node.left == null && node.right == null) {
+            return node.data;
+        }
+
+        val right = node.right;
+        val left = node.left;
+
+        val leftSum = if (left!=null)  maxSumToRootv2(left) else 0
+        val rightSum = if (right!=null)  maxSumToRootv2(right) else 0
+
+        val tmpMax = Math.max(Math.max(leftSum,rightSum), leftSum+rightSum+node.data)
+
+        if (tmpMax>max) max=tmpMax
+
+        return Math.max(
+                leftSum+ node.data,
+                rightSum + node.data)
+
+    }
+
+    fun maxSumToRoot(node: BinaryTree<Int>): Int {
 
         if (node.left == null && node.right == null) {
             return node.data;
