4fe1cb9ba09dd8113f0fe51d5bfc4e83041ddbbe
eleco <eric.leconte@gmail.com>
2018-07-25 21:24:08 +0100

    problem88 division

---
+++ src/problem88/Main.kt
@@ -0,0 +1,25 @@
+package problem88
+
+//Implement division of two positive integers without using the division, multiplication, or modulus operators.
+// Return the quotient as an integer, ignoring the remainder.
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        divide(35, 7)
+        divide(35, 6)
+    }
+
+    fun divide(divisor: Int, dividend: Int) {
+        var quotient = 0;
+
+        var tmpDivisor = divisor
+
+        while (tmpDivisor >= dividend) {
+            tmpDivisor -= dividend;
+            quotient++;
+        }
+        val remainder = divisor - (quotient * dividend);
+        println("divisor $divisor dividend $dividend quotient $quotient remainder $remainder")
+    }
+}
